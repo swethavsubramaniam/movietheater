81b993b25381087cd7e92b7a77abc0c6631f3e55
swethavsubramaniam <swetha.visesh@gmail.com>
2022-11-28 15:52:34 -0500

    Modified code:
    Implemented new features below:
    ## New Requirements
    * New discount rules; In addition to current rules
      * Any movies showing starting between 11AM ~ 4pm, you'll get 25% discount
      * Any movies showing on 7th, you'll get 1$ discount
      * The discount amount applied only one if met multiple rules; biggest amount one
    * We want to print the movie schedule with simple text & json format
    
    Eliminated unused code
    Changed code where logic was incorrect as per requirements

---
+++ orig/src/test/java/com/jpmc/theater/CustomerTests.java
@@ -0,0 +1,22 @@
+package com.jpmc.theater;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CustomerTests {
+
+    @Test
+    void testCustomerInfo() {
+        Customer customer1 = new Customer("John Doe", "123");
+        Customer customer2 = new Customer("John Doe", "321");
+        Movie spiderMan1 = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, 1);
+        assertTrue(customer1.equals(customer1));
+        assertFalse(customer2.equals(customer1));
+        assertFalse(customer2.equals(spiderMan1));
+        assertFalse(customer2.hashCode() == customer1.hashCode());
+        assertEquals("name: John Doe", customer1.toString());
+    }
+}
